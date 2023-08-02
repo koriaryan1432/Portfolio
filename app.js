a02b88c4aad79ddbdf8b74ddce78956dba0e1dbf
koriaryan1432 <koriaryan@gmail.com>
2023-08-02 13:04:09 +0530

    Javascript work

--- app.js
+++ app.js
@@ -1,7 +1,38 @@
-var typed= new(".text",{
-  String:["Full Stack Developer", "Tech Enthusiast","Graphic Designer"],
-  typeSpeed:100,
-  backSpeed:100,
-  backDelay:1000,
-  loop:true
-})
+console.log('hi')
+
+// toggle icon navbar
+
+let menuIcon = document.querySelector('#Menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () =>{
+      menuIcon.classList.toggle('bx-x');
+      navbar.classList.toggle('active');
+};
+// scroll section acive link
+ 
+
+let sections =document.querySelectorAll('section');
+let navlinks =document.querySelectorAll('header nav  a');
+
+window.onscroll = () => {
+  sections.forEach(sec => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop -150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute('id');
+
+    if(top >=offset && top < offset + height)
+    {
+      navlinks.forEach(links => {
+        links.classList.remove('active');
+        document.querySelector("header nav a[href*=" + id + "]").classList.add('active');
+      });
+    };
+  });
+  let header = document.querySelector('header');
+  header.classList.toggle('sticky',window.screenY > 100);
+
+};
+
+
